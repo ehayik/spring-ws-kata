81218b2702a80635ee664122ebb2d07dc43f5392
Eduardo Eljaiek <14299643+ehayik@users.noreply.github.com>
2024-08-10 23:00:54 +0300

    Add Spring WS server module
    Introduce a new Spring Boot-based web services server module. This includes the main application class, configuration file, unit tests, and Maven setup. Additionally, update the root POM to include this new module.

---
+++ spring-ws-server/src/main/java/io/github/ehayik/kata/springwsserver/Application.java
@@ -0,0 +1,13 @@
+package io.github.ehayik.kata.springwsserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
